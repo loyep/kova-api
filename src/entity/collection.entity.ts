adf4245829ccd9a5c61104104fd4cdff88b9f386
Zack <mwl@live.com>
2020-12-23 01:34:23 +0800

    更新

---
+++ src/entity/collection.entity.ts
@@ -0,0 +1,15 @@
+import { CreateDateColumn, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'collections' })
+export class Collection {
+  @PrimaryGeneratedColumn('increment', { type: 'bigint' })
+  id: number;
+  // @PrimaryColumn('varchar')
+  // type: string;
+  // @PrimaryColumn('bigint', { unsigned: true })
+  // like_id: number;
+  // @PrimaryColumn('bigint', { unsigned: true })
+  // user_id: number;
+  // @CreateDateColumn({ name: 'created_at' })
+  // createdAt: Date;
+}
