294a9a4be2c8a985e90b7af130ee77f22f10a2c9
Zack <mwl@live.com>
2021-02-02 17:41:01 +0800

    更新

--- src/core/interceptors/logging.interceptor.ts
+++ src/core/interceptors/logging.interceptor.ts
@@ -1,19 +1,23 @@
-import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from "@nestjs/common"
+import { Observable } from "rxjs"
+import { tap } from "rxjs/operators"
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  private readonly logger = new Logger(LoggingInterceptor.name, true);
+  private readonly logger = new Logger(LoggingInterceptor.name, true)
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    const request = context.switchToHttp().getRequest();
-    const { url, method, params, query, headers } = request;
+    const request = context.switchToHttp().getRequest()
+    const { url, method, params = {}, query = {}, headers } = request
 
-    this.logger.log(`Before: ${method} ${url} with :
-     params: ${JSON.stringify(params)}, with query: ${JSON.stringify(query)}`);
+    try {
+      this.logger.log(`Before: ${method} ${url} with :
+    params: ${JSON.stringify(params)}, with query: ${JSON.stringify(query)}`)
+    } catch (error) {
+      console.log(request)
+    }
 
-    const now = Date.now();
-    return next.handle().pipe(tap(() => this.logger.log(`After: ${method} ${url} took ${Date.now() - now}ms`)));
+    const now = Date.now()
+    return next.handle().pipe(tap(() => this.logger.log(`After: ${method} ${url} took ${Date.now() - now}ms`)))
   }
 }
